5661cafadecaa543c5e3b60ba420a8408bcfcc41
Cooya <nico@cooya.fr>
2018-08-24 12:46:02 +0200

    xml parser added

--- index.js
+++ index.js
@@ -2,26 +2,41 @@ const sleep = require('sleep');
 
 const config = require('./config');
 const pup = require('./pup_utils');
+const reader = require('./reader');
 
 const startingPointUrl = 'https://www.facebook.com/marketplace/';
 
 (async function main() {
+
+    // read input XML file
+    if(!config.inputFile)
+        throw Error('Missing input file declaration in config file.')
+    const xml = await reader.readXMLFile(config.inputFile);
+    const items = xml.rss.channel[0].item;
+    if(!items)
+        throw Error('No item to sell.');
+
+    // open browser and load cookies
     const browser = await pup.runBrowser({headless: false});
     const page = await pup.createPage(browser, config.cookiesFile);
     await pup.loadCookies(page, config.cookiesFile);
 
+    // go to the marketplace and log in if needed
     await pup.goTo(page, startingPointUrl);
-
     const loginForm = await page.$('#login_form');
     if(loginForm) {
+        if(!config.login || !config.password)
+            throw Error('Missing credential declaration in the config file.');
         await logIn(page, config.login, config.password);
         await page.waitForNavigation();
         await sleep.sleep(1);
         await pup.saveCookies(page, config.cookiesFile);
     }
 
-    await clickOnSellButton(page);
-    await fillSellForm(page, 'toto', '50', 'Cars, Trucks & Motorcycles', 'salut les gens');
+    for(let item of items) {
+        await clickOnSellButton(page);
+        await fillSellForm(page, 'toto', '50', 'Cars, Trucks & Motorcycles', 'salut les gens');
+    }
 
     //await browser.close();
 })();
