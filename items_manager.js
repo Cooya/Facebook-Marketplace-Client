e312793b4b8f138b6b0e393b1946d9f80dadd373
Nicolas <nico@cooya.fr>
2018-08-31 00:05:03 +0200

    scalability of items manager improved

--- items_manager.js
+++ items_manager.js
@@ -7,67 +7,80 @@ const utils = require('./utils');
 const db = new loki(config.dbFile, {verbose: true});
 const loadDatabase = util.promisify(db.loadDatabase.bind(db));
 const saveDatabase = util.promisify(db.saveDatabase.bind(db));
+const requiredKeys = ['link', 'title', 'price', 'location', 'description', 'pictures'];
 let itemsCollection;
 
 async function loadItems() {
     await loadDatabase({});
 
+    // load or create the items collection
     itemsCollection = db.getCollection('items');
     if(!itemsCollection)
         itemsCollection = db.addCollection('items', {unique: 'link'});
 
-    // read input XML file
+    // if input file exists
     if(config.inputFile && await utils.fileExists(config.inputFile)) {
+
+        // read xml file
         const xml = await utils.readXMLFile(config.inputFile);
-        const items = xml.xml.annonce;
-        const requiredKeys = ['link', 'title', 'price', 'location', 'description', 'pictures'];
 
         // process items
-        console.log(items.length + ' items to process.');
-        const processedItems = await Promise.all(items.map(async (item) => {
-            const processedItem = {};
-            processedItem.link = item.lien[0];
-            processedItem.title = item.type[0];
-            processedItem.price = item.prix[0].replace(',00 EUR', '');
-            processedItem.location = item.ville && item.ville[0];
-            processedItem.description = item.descriptif[0];
-            processedItem.category = config.itemCategory;
-               
-            if(item.photos) {
-                processedItem.pictures = [];
-                for(let photo of item.photos[0].photo)
-                    processedItem.pictures.push(await utils.downloadFile(photo, config.picturesFolder));
-            }
+        const processedItems = await processItems(xml.xml.annonce, requiredKeys);
 
-            for(let key of requiredKeys) {
-                if(!processedItem[key]) {
-                    console.error('Processed item is invalid : "' + processedItem.link + '", missing key "' + key + '".');
-                    return null;
-                }
-            }
-
-            return processedItem;
-        }));
-
-        let counter = 0;
-        for(let item of processedItems) {
-            if(!item)
-                continue;
-            if(itemsCollection.findOne({link: item.link}))
-                ;//console.warn('Item "' + item.link + '" already exists in database.');
-            else {
-                item.processed = false;
-                itemsCollection.insert(item);
-                //console.log('Item "' + item.link + '" inserted into database.');
-            }
-            counter++;
-        }
-        
-        await saveDatabase();
-        console.log(counter + ' new items loaded into database.');
+        // save processed items into database       
+        await saveItemsIntoDatabase(processedItems);
     }
 
+    // return the unprocessed items from the database
     return itemsCollection.find({processed: false});
+}
+
+async function processItems(items, requiredKeys = []) {
+    console.log(items.length + ' items to process.');
+    return await Promise.all(items.map(async (item) => {
+        const processedItem = {};
+        processedItem.link = item.lien[0];
+        processedItem.title = item.type[0];
+        processedItem.price = item.prix[0].replace(',00 EUR', '');
+        processedItem.location = item.ville && item.ville[0];
+        processedItem.description = item.descriptif[0];
+        processedItem.category = config.itemCategory;
+            
+        if(item.photos) {
+            processedItem.pictures = [];
+            for(let photo of item.photos[0].photo)
+                processedItem.pictures.push(await utils.downloadFile(photo, config.picturesFolder));
+        }
+
+        for(let key of requiredKeys) {
+            if(!processedItem[key]) {
+                console.error('Processed item is invalid : "' + processedItem.link + '", missing key "' + key + '".');
+                //console.error(processedItem);
+                return null;
+            }
+        }
+
+        return processedItem;
+    }));
+}
+
+async function saveItemsIntoDatabase(items) {
+    let counter = 0;
+    for(let item of items) {
+        if(!item)
+            continue;
+        if(itemsCollection.findOne({link: item.link}))
+            ;//console.warn('Item "' + item.link + '" already exists in database.');
+        else {
+            item.processed = false;
+            itemsCollection.insert(item);
+            //console.log('Item "' + item.link + '" inserted into database.');
+        }
+        counter++;
+    }
+    
+    await saveDatabase();
+    console.log(counter + ' new items loaded into database.');
 }
 
 async function markItemAsProcessed(itemLink) {
