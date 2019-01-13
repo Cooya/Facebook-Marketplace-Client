08285b171b9b7cd50313aff868e855693e5aec8c
Nicolas <nico@cooya.fr>
2019-01-13 15:44:17 +1300

    get action added

--- src/index.js
+++ src/index.js
@@ -7,11 +7,14 @@ const Launcher = require('./launcher');
 
 	const args = process.argv.slice(2);
 	let action = null;
+	let id = null;
 	for (let i in args) {
 		i = parseInt(i);
-		if (args[i] == '--action' && i < args.length)
+		if (args[i] == '--action' && i < args.length) {
 			action = args[i + 1];
-		if (args[i] == '--interval' && i < args.length) {
+		} else if (args[i] == '--id' && i < args.length) {
+			id = args[i + 1];
+		} else if (args[i] == '--interval' && i < args.length) {
 			config.intervalBetweenActions = [args[i + 1], args[i + 1]];
 			console.log('Interval between each action set up to % seconds.', args[i + 1]);
 		}
@@ -20,10 +23,16 @@ const Launcher = require('./launcher');
 	try {
 		const launcher = new Launcher();
 		await launcher.itemsManager.connect();
-		await launcher.run(action);
+		if (action != 'get') {
+			await launcher.run(action);
+		} else {
+			if (!id) throw new Error('No item id provided.');
+			const item = await launcher.itemsManager.getItem(id);
+			if (item) console.log(item);
+			else console.log('The item has not been found.');
+		}
 		await launcher.itemsManager.end();
-	}
-	catch(e) {
+	} catch (e) {
 		console.error(e);
 		process.exit(0); // 0 allows to avoid NPM verbose output
 	}
