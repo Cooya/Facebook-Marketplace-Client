ace4bcfb9ba414c89a248c216b958a6d15e29c6a
Nicolas <nico@cooya.fr>
2019-01-13 15:19:26 +1300

    parseAndCheckLogin error handled but not fixed

--- src/chat_server.js
+++ src/chat_server.js
@@ -3,37 +3,53 @@ const login = require('facebook-chat-api');
 
 const config = require('../config');
 
-const REDIRECTION_ANSWER = 'Bonjour, merci de votre message. Je vous invite à échanger sur ma page {chatbot_url}';
+const REDIRECTION_ANSWER =
+	'Bonjour, merci de votre message. Je vous invite à échanger sur ma page {chatbot_url}';
 const CHATBOT_URL = 'https://m.me/327212517859391?ref=w4181902--{item_id}';
+
+// to handle the parseAndCheckLogin error
+process.on('unhandledRejection', error => {
+	console.debug('UNHANDLED REJECTION');
+	console.error(error);
+});
 
 (async () => {
 	try {
 		const appStateFile = fs.existsSync(config.appStateFile);
-		const creds = appStateFile ? { appState: JSON.parse(fs.readFileSync(config.appStateFile, 'utf8')) } : {email: config.login, password: config.password};
+		const creds = appStateFile
+			? { appState: JSON.parse(fs.readFileSync(config.appStateFile, 'utf8')) }
+			: { email: config.login, password: config.password };
 
 		login(creds, (err, api) => {
-			if(err) throw err;
+			if (err) {
+				console.error('An error occurred while trying to log in.');
+				throw err;
+			}
 
 			fs.writeFileSync(config.appStateFile, JSON.stringify(api.getAppState()));
 
 			api.listen((err, message) => {
-				if (err) throw err;
+				if (err) {
+					console.error('An error occurred while trying to listen messages.');
+					throw err;
+				}
 				//console.debug(message);
 
 				api.getThreadInfo(message.threadID, (err, info) => {
-					if(err) throw (err);
+					if (err) {
+						console.error('An error occurred while trying to get the thread info.');
+						throw err;
+					}
 					//console.debug(info);
 
 					const itemId = /\[([0-9]+)\]/.exec(info.threadName);
-					if(!itemId)
-						return;
+					if (!itemId) return;
 
 					api.sendMessage(buildAnswer(itemId[1]), message.threadID);
 				});
 			});
 		});
-	}
-	catch(e) {
+	} catch (e) {
 		console.error(e);
 	}
 })();
