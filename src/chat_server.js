5e0da45506656d04eb11b822356acb8a7e19a54f
Nicolas <nico@cooya.fr>
2019-04-03 20:01:35 +0200

    login with corrupted appstate file fixed

--- src/chat_server.js
+++ src/chat_server.js
@@ -13,26 +13,30 @@ process.on('unhandledRejection', error => {
 	console.error(error);
 });
 
+let loginAttempts = 0;
+
 (() => {
 	try {
-		const appStateFile = fs.existsSync(config.appStateFile);
-		const creds = appStateFile
-			? { appState: JSON.parse(fs.readFileSync(config.appStateFile, 'utf8')) }
-			: { email: config.login, password: config.password };
+		const appStateFileExists = fs.existsSync(config.appStateFile);
 
-		login(creds, (err, api) => {
+		const loginCallback = (err, api) => {
 			if (err) {
 				console.error('An error occurred while trying to log in.');
-				throw err;
+				if(/Error retrieving userID/.test(err.error) && ++loginAttempts == 0) {
+					login(getCreds(false), loginCallback);
+					return;
+				}
+				else
+					throw err.error;
 			}
-
+		
 			api.setOptions({
 				logLevel: 'silly',
 				selfListen: true
 			});
-
+		
 			fs.writeFileSync(config.appStateFile, JSON.stringify(api.getAppState()));
-
+		
 			let listenCounter = 0;
 			const listen = () => {
 				listenCounter++;
@@ -48,31 +52,38 @@ process.on('unhandledRejection', error => {
 							sleep.sleep(10);
 							listen();
 							return;
-						} else throw err;
+						} else throw err.error;
 					}
 					//console.debug(message);
-
+		
 					api.getThreadInfo(message.threadID, (err, info) => {
 						if (err) {
 							console.error('An error occurred while trying to get the thread info.');
-							throw err;
+							throw err.error;
 						}
 						//console.debug(info);
-
+		
 						const itemId = /\[([0-9]+)\]/.exec(info.threadName);
 						if (!itemId) return;
-
+		
 						api.sendMessage(buildAnswer(itemId[1]), message.threadID);
 					});
 				});
 			};
 			listen();
-		});
+		};
+		login(getCreds(appStateFileExists), loginCallback);
 	} catch (e) {
 		console.error('An error has been caught.');
 		console.error(e);
 	}
 })();
+
+function getCreds(useAppStateFile) {
+	return useAppStateFile
+		? { appState: JSON.parse(fs.readFileSync(config.appStateFile, 'utf8')) }
+		: { email: config.login, password: config.password };
+}
 
 function buildAnswer(itemId) {
 	return REDIRECTION_ANSWER.replace('{chatbot_url}', CHATBOT_URL.replace('{item_id}', itemId));
