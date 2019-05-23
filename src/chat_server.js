f00bedad3aca3a0f0911270ce288d7849f7d20ac
Nicolas <nico@cooya.fr>
2019-05-23 12:01:05 +0200

    chat server greatly improved

--- src/chat_server.js
+++ src/chat_server.js
@@ -1,89 +1,102 @@
+const facebookChatApi = require('facebook-chat-api');
 const fs = require('fs');
-const login = require('facebook-chat-api');
-const sleep = require('sleep');
 
 const config = require('../config');
 const logger = require('./logger');
 
-const REDIRECTION_ANSWER =
-	'Bonjour, merci de votre message. Je vous invite à échanger sur ma page {chatbot_url}';
+const REDIRECTION_ANSWER = 'Bonjour, merci de votre message. Je vous invite à échanger sur ma page {chatbot_url}';
 const CHATBOT_URL = 'https://m.me/327212517859391?ref=w4181902--{item_id}';
 
 process.on('unhandledRejection', error => {
-	logger.debug('UNHANDLED REJECTION');
+	logger.error('UNHANDLED REJECTION');
 	logger.error(error);
 });
 
-let loginAttempts = 0;
-
-(() => {
+(async () => {
+	const appStateFileExists = fs.existsSync(config.appStateFile);
+	let loginAttempts = 0;
+	let api;
+	
+	// log in to the API
 	try {
-		const appStateFileExists = fs.existsSync(config.appStateFile);
-
-		const loginCallback = (err, api) => {
-			if (err) {
-				logger.error('An error occurred while trying to log in.');
-				if(/Error retrieving userID/.test(err.error) && ++loginAttempts == 0) {
-					login(getCreds(false), loginCallback);
-					return;
-				}
-				else
-					throw err.error;
-			}
-		
-			api.setOptions({
-				logLevel: 'silly',
-				selfListen: true
-			});
-		
-			fs.writeFileSync(config.appStateFile, JSON.stringify(api.getAppState()));
-		
-			let listenCounter = 0;
-			const listen = () => {
-				listenCounter++;
-				api.listen((err, message) => {
-					if (err) {
-						logger.error('An error occurred while trying to listen messages.');
-						if (
-							err.message ==
-								'parseAndCheckLogin got status code: 200. Bailing out of trying to parse response.' &&
-							listenCounter <= 5
-						) {
-							logger.info('Tryin again in 10 seconds...');
-							sleep.sleep(10);
-							listen();
-							return;
-						} else throw err.error;
-					}
-					//logger.debug(message);
-		
-					api.getThreadInfo(message.threadID, (err, info) => {
-						if (err) {
-							logger.error('An error occurred while trying to get the thread info.');
-							throw err.error;
-						}
-						//logger.debug(info);
-		
-						const itemId = /\[([0-9]+)\]/.exec(info.threadName);
-						if (!itemId) return;
-		
-						api.sendMessage(buildAnswer(itemId[1]), message.threadID);
-					});
-				});
-			};
-			listen();
-		};
-		login(getCreds(appStateFileExists), loginCallback);
-	} catch (e) {
-		logger.error('An error has been caught.');
+		api = await login(appStateFileExists, loginAttempts);
+	} catch(e) {
 		logger.error(e);
+		process.exit(1);
 	}
+
+	// update the app state file
+	fs.writeFileSync(config.appStateFile, JSON.stringify(api.getAppState()));
+
+	// configure the API
+	api.setOptions({
+		logLevel: 'info',
+		selfListen: false // set it to true may be dangerous
+	});
+		
+	api.listen(async (err, message) => {
+		if (err) {
+			logger.error('An error has occurred while listening for messages.');
+			logger.error(err);
+		}
+		else {
+			logger.info('Message received !');
+			try {
+				const itemId = await getItemId(api, message);
+				if(!itemId) logger.warning('No response sent because the item id has not been found in this message.');
+				else {
+					await sendMessage(api, itemId, message.threadID);
+					logger.info('Message sent successfully.');
+				}
+			} catch(e) {
+				console.error(e);
+			}
+		}
+	});
 })();
 
 function getCreds(useAppStateFile) {
 	return useAppStateFile
 		? { appState: JSON.parse(fs.readFileSync(config.appStateFile, 'utf8')) }
 		: { email: config.login, password: config.password };
+}
+
+function login(appStateFileExists, loginAttempts) {
+	return new Promise((resolve, reject) => {
+		const creds = getCreds(appStateFileExists);
+		facebookChatApi(creds, (err, api) => {
+			if (err) {
+				logger.error('An error has occurred while trying to log in.');
+				if(/Error retrieving userID/.test(err.error) && ++loginAttempts == 0)
+					login(appStateFileExists, loginAttempts).then(resolve, reject);
+				else
+					reject(err);
+			} else resolve(api);
+		});
+	});
+}
+
+function getItemId(api, message) {
+	return new Promise((resolve, reject) => {
+		api.getThreadInfo(message.threadID, (err, info) => {
+			if (err) {
+				logger.error('An error has occurred while trying to get the thread info.');
+				reject(err);
+			} else {
+				const itemId = /\[([0-9]+)\]/.exec(info.threadName);
+				resolve(itemId ? itemId[1] : null);
+			}
+		});
+	});
+}
+
+function sendMessage(api, itemId, threadId) {
+	return new Promise((resolve, reject) => {
+		api.sendMessage(buildAnswer(itemId), threadId, (err) => {
+			if(err) reject(err);
+			else resolve();
+		});
+	});
 }
 
 function buildAnswer(itemId) {
